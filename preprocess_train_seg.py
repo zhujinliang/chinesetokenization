4a312ed840983e7dddbecdfc7608c48aaa889ad9
Jinliang Zhu <zhujinlianghust@gmail.com>
2013-04-30 15:32:51 +0800

    Add function to preprocess tain_seg.txt

--- preprocess_train_seg.py
+++ preprocess_train_seg.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-f = open('test.txt')
-lines = f.readlines()
+def add_start_end(file_name):
+    f = file(file_name)
+    lines = f.readlines()
+    f.close()
+    for i in range(len(lines)):
+        lines[i] = 's ' + lines[i][0:-2] + ' e ' + lines[i][-1]
+    p_file = file('p_train_seg.txt', 'w')
+    p_file.writelines(lines)
+    p_file.close()
 
-print lines[3]
+if __name__ == '__main__':
+    add_start_end('train_seg.txt')
