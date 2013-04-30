1ba06d76d9720cdda9e06199503dab9a7575fb24
Jinliang Zhu <zhujinlianghust@gmail.com>
2013-05-01 01:05:59 +0800

    Update pro_dict

--- pro_dict.py
+++ pro_dict.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import re
 
 class ProDict(object):
     ''' Get train set for tokenization. '''
@@ -8,23 +9,43 @@ class ProDict(object):
 
     def __init__(self, train_file):
         self.train_file = train_file
+        self.construct_pro_dict()
 
     def is_vocable(self, vocable):
         if vocable in self.pro_dict:
             return True
         else:
             return False
-    
+
+    def preprocess_train_file(self, train_file):
+        f = file(train_file)
+        lines = f.readlines()
+        f.close()
+        for i in range(len(lines)):
+            lines[i] = 's  ' + lines[i][0:-2] + ' e ' + lines[i][-1]
+        return lines
+
     def construct_pro_dict(self):
         f = file(self.train_file)
-        sentences = f.readlines()
+        lines = f.readlines()
         f.close()
+        re_han = re.compile(ur'([\u4E00-\u9FA5\s]+)')
+        re_skip = re.compile(ur'([\.0-9]+|[a-zA-Z0-9]+)')
+        re_eng = re.compile(ur'[a-zA-Z0-9]+')
+        re_num = re.compile(ur'[\.0-9]+')
         all_words = []
-        for s in sentences:
-            words = s.split('  ')
-            all_words.append(words)
+        sen_words = []
+        for line in lines:
+            sentences = re_han.split(line.strip(' '))
+            length = len(sentences)
+            for i in range(length):
+                sentences[i] = 's  ' + sentences[i] + 'e'
+            for s in sentences:
+                words = s.split('  ')
+                sen_words.append(words)
+                all_words.extend(words)
 
 
 
 if __name__ == '__main__':
-    d = ProDict('p_train_seg.txt')
+    d = ProDict('train_seg.txt')
